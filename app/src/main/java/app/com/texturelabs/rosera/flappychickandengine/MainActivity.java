e2fb25f5a82e41b5abd71a5107bea57666b7d502
rosie <technologynorth@gmail.com>
2015-04-16 18:50:55 +0100

    initial commit - FlappyChick AndEngine skeleton code for Android Studio

---
+++ app/src/main/java/app/com/texturelabs/rosera/flappychickandengine/MainActivity.java
@@ -0,0 +1,26 @@
+package app.com.texturelabs.rosera.flappychickandengine;
+
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.entity.scene.Scene;
+
+public class MainActivity extends SimpleBaseGameActivity {
+
+    @Override
+    public EngineOptions onCreateEngineOptions()
+    {
+        return null;
+    }
+
+    @Override
+    protected void onCreateResources()
+    {
+
+    }
+
+    @Override
+    protected Scene onCreateScene()
+    {
+        return null;
+    }
+}
